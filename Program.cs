fd390b551d117ac041a659954e64a7b6aade34f6
tFury <thomas_haenig@gmx.de>
2018-06-03 22:26:12 +0200

    set up project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repository_service_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
